6c74041db6f579c6685b02df421333d6743666fc
James Pecore <jjp2196@columbia.edu>
2023-06-24 17:34:54 -0400

    Place file in correct location

---
+++ abstract_classes/abstract_classes_java/AbstractStack.java
@@ -0,0 +1,25 @@
+package abstract_classes.abstract_classes_java;
+public abstract class AbstractStack {
+    protected int size;
+
+    // Constructor for AbstractStack
+    public AbstractStack() {
+        size = 0;
+    }
+
+    // Abstract method for pushing an element to the stack
+    public abstract void push(int value);
+
+    // Abstract method for popping an element from the stack
+    public abstract int pop();
+
+    // Returns the current size of the stack
+    public int size() {
+        return size;
+    }
+
+    // Checks if the stack is empty
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
