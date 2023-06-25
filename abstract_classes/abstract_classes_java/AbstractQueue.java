95f56ad4d9d1930f8d8db80e44d5082b0efe4673
James Pecore <jjp2196@columbia.edu>
2023-06-25 11:54:26 -0400

    Added more comments to clarify the code

--- abstract_classes/abstract_classes_java/AbstractQueue.java
+++ abstract_classes/abstract_classes_java/AbstractQueue.java
@@ -3,18 +3,23 @@ package abstract_classes.abstract_classes_java;
 public abstract class AbstractQueue {
     protected int size;
 
+    // Abstract Queue initializing object
     public AbstractQueue() {
         size = 0; 
     }
 
+    // Enqueue (adding to front of line)
     public abstract void enqueue(int value); 
 
+    // Dequeue (removing from front of line)
     public abstract int dequeue(); 
 
+    // Returns the value for size, int type
     public int size() {
         return size;
     }
 
+    // Returns T/F if queue is empty, good for while loops
     public boolean isEmpty() {
         return size == 0;
     }
