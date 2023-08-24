54ce3cb767c281742bdac60580ae704f3a5d68d3
James Pecore <jjp2196@columbia.edu>
2023-08-24 16:15:37 -0400

    Creating visualization app with BSTs

---
+++ visualizations/bst-visualization/src/BST.js
@@ -0,0 +1,20 @@
+// src/BST.js
+import React, { Component } from 'react';
+
+class BST extends Component {
+  constructor(props) {
+    super(props);
+    // Initialize BST state or logic here
+  }
+
+  render() {
+    // Render BST nodes, manage state, and handle user interactions
+    return (
+      <div>
+        {/* Render your BST visualization here */}
+      </div>
+    );
+  }
+}
+
+export default BST;
